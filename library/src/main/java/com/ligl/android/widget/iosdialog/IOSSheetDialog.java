510199c4c17d530fd126f0277309c2c3741b81b0
itlgl <ligl6688@gmail.com>
2017-08-16 19:20:54 +0800

    修改项目的build文件，将aar打包上传到jcenter
    有一个比较坑的问题需要注意一下，网上的教程有错误的地方，具体在project/build.gradle的配置问题上，参考http://blog.csdn.net/qiang_xi/article/details/52777358，将classpath 'com.github.dcendents:android-gradle-plugin:1.5'修改为classpath 'com.github.dcendents:android-maven-gradle-plugin:1.5'

--- library/src/main/java/com/ligl/android/widget/iosdialog/IOSSheetDialog.java
+++ library/src/main/java/com/ligl/android/widget/iosdialog/IOSSheetDialog.java
@@ -10,7 +10,6 @@ import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.ViewGroup.LayoutParams;
